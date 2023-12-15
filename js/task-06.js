a7ecaf4d709a4dfeb17180d4d0ce8da3b0d7fb23
aleksandrgarachun <115419603+aleksandrgarachun@users.noreply.github.com>
2023-12-15 18:45:10 +0300

    6 task

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,52 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input")
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+
+
+
+create.addEventListener("click", createCube);
+destroy.addEventListener("click", destroyCube);
+
+function createCube () {
+  const elem = parseInt(input.value);
+  
+  if (!elem || elem < 1 || elem > 100) {
+    return;
+  }
+
+  boxes.innerHTML = "";
+
+  let size = 30;
+
+  for(let i=0; i < elem; i += 1) {
+    const cube = document.createElement("div");
+    cube.style.height = `${size}px`;
+    cube.style.width = `${size}px`;
+    cube.style.backgroundColor = getRandomHexColor();
+    boxes.append(cube); 
+    size += 10;
+  }
+
+
+
+
+
+  input.value = '';
+}
+
+
+function destroyCube () {
+  boxes.innerHTML = "";
+}
+
+
+
+
+
+
